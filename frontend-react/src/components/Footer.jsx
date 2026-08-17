3aa13e143d11f32fbc294c1add1cf28a24698df3
kaushik gowda <kaushik942003@gmail.com>
2026-08-17 11:06:22 +0530

    home page react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer py-3 my-3'>
+            <hr className='border-bottom'></hr> 
+            <p className='text-light text-center'> &copy; Made by Kaushik Gowda M</p>   
+        </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Prediction Portal</h1>
+                <p className='text-light lead'>Smart Stock Insights, Powered by AI. Analyze historical trends, evaluate market performance, and preview data-backed price predictions to make smarter, faster financial moves.</p>
+                <Button text="Login" class="btn-info"></Button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
