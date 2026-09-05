e2e432708ae70602097019879a5cfce0f68bb3b5
kaushik gowda <kaushik942003@gmail.com>
2026-09-05 13:15:32 +0530

    small bug fixed

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, {useState, useContext} from 'react'
 import {useNavigate} from 'react-router-dom'
-import { AuthContext } from '../authProvider'
+import { AuthContext } from '../AuthProvider'
 
 const Login = () => {
 
